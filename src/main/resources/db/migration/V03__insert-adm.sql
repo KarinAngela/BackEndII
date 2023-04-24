5c66a0ee4d1e210ab1fef82e1b1c57e5c3a1e561
KarinAngela <kahnogueira0407@gmail.com>
2023-04-24 00:05:21 -0300

    Corrige erros nos testes unitarios

---
+++ src/main/resources/db/migration/V03__insert-adm.sql
@@ -0,0 +1,17 @@
+insert into usuarios (
+    id,
+    nome,
+    email,
+    login,
+    senha,
+    data_cadastro,
+    nivel_acesso
+
+ ) values (
+ 0,
+ "admMaster",
+  "admmaster@gmail.com",
+  "adminMaster",
+  "$2a$04$y2BjhXtVGHMY8tYL2TPCO.0/igC3qUCQAqhAMkPnsj5r65e7MNnC.",
+  '2023-04-04',
+  "ADMINISTRADOR");
